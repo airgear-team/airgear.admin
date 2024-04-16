aeec9c557633af05fba05065f81f5093d1037394
Olena Sinkevych <fuegos1981@gmail.com>
2024-04-16 11:01:34 +0300

    user search

--- src/main/java/com/airgear/admin/controller/UserController.java
+++ src/main/java/com/airgear/admin/controller/UserController.java
@@ -7,13 +7,11 @@ import com.airgear.admin.service.UserService;
 import io.swagger.v3.oas.annotations.Parameter;
 import org.springdoc.core.converters.models.PageableAsQueryParam;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -180,7 +178,6 @@ public class UserController {
         userService.deleteById(id);
     }
 
-    @PreAuthorize("hasAnyRole('ADMIN','MODERATOR')")
     @RequestMapping(value = "/find", method = RequestMethod.GET)
     public Page<UserSearchResponse> searchUsers(@RequestParam(value = "search") String search,
                                                 @PageableDefault(size = 30) Pageable pageable) {
